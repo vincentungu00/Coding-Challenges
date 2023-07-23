8cae2a9086d5df7754bc8c92c92fdfbcb1b7f98e
vincent.ungu <vincentungu.work@gmail.com>
2023-07-23 19:29:27 +0700

    Create Weather Observation Station 1.sql

---
+++ HackerRank/SQL/Weather Observation Station 1.sql
@@ -0,0 +1,11 @@
+/*
+Link: https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+
+Question:
+Query a list of CITY and STATE from the STATION table.
+
+Solution in MySQL.
+*/
+
+SELECT s.CITY, s.STATE
+FROM STATION AS s;
