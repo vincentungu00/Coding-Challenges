7c1630c8755667224bfaea99113720f4c550530f
vincent.ungu <vincentungu.work@gmail.com>
2023-07-23 19:06:50 +0700

    Create Select All.sql

---
+++ HackerRank/SQL/Select All.sql
@@ -0,0 +1,11 @@
+/*
+Link: https://www.hackerrank.com/challenges/select-all-sql/problem
+
+Question:
+Query all columns (attributes) for every row in the CITY table.
+
+Solution in MySQL.
+*/
+
+SELECT c.*
+FROM CITY AS c;
