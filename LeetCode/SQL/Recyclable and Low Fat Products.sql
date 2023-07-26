6b78f4e0b08d28f4241f5d4dd0cb6f00f230cbbc
vincent.ungu <vincentungu.work@gmail.com>
2023-07-26 22:13:57 +0700

    Create Recyclable and Low Fat Products.sql

---
+++ LeetCode/SQL/Recyclable and Low Fat Products.sql
@@ -0,0 +1,13 @@
+/*
+Link: https://leetcode.com/problems/recyclable-and-low-fat-products
+
+Question:
+Find the ids of products that are both low fat and recyclable. Return the result table in any order.
+
+Solution in MySQL.
+*/
+
+SELECT product_id
+FROM Products AS p
+WHERE p.low_fats = "Y"
+AND p.recyclable = "Y";
