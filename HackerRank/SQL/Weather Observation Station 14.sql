87d13597a3b1e58851ec41af4d6b2c34b4a206dd
vincent.ungu <vincentungu.work@gmail.com>
2023-08-03 22:38:08 +0700

    Update Weather Observation Station 14.sql

--- HackerRank/SQL/Weather Observation Station 14.sql
+++ HackerRank/SQL/Weather Observation Station 14.sql
@@ -7,6 +7,6 @@ Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is 
 Solution in MySQL.
 */
 
-SELECT ROUND(MAX(s.LAT_N), 4) AS sum_lat_n
+SELECT ROUND(MAX(s.LAT_N), 4) AS max_lat_n
 FROM STATION AS s
 WHERE lat_n < 137.2345;
