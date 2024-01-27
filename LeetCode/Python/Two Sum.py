5a27bb232dee923d42117acfb55a975c54dee7f2
vincent.ungu <vincentungu.work@gmail.com>
2024-01-27 21:47:32 +0700

    Update Two Sum.py

--- LeetCode/Python/Two Sum.py
+++ LeetCode/Python/Two Sum.py
@@ -1,5 +1,5 @@
 """
-Link: https://leetcode.com/problems/two-sum
+Link: https://leetcode.com/problems/two-sum/
 
 Question:
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
