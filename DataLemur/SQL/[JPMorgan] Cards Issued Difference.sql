15ed9b3a11880e24031518907be5eb46dc52aca4
vincentjunitio00 <vincent.50808@gmail.com>
2024-09-08 09:42:11 +0700

    Create [JPMorgan] Cards Issued Difference.sql

---
+++ DataLemur/SQL/[JPMorgan] Cards Issued Difference.sql
@@ -0,0 +1,16 @@
+/*
+Link: https://datalemur.com/questions/cards-issued-difference
+
+Question:
+Your team at JPMorgan Chase is preparing to launch a new credit card, and to gain some insights, you're analyzing how many credit cards were issued each month.
+
+Write a query that outputs the name of each credit card and the difference in the number of issued cards between the month with the highest issuance cards and the lowest issuance. Arrange the results based on the largest disparity.
+
+Solution in PostgreSQL.
+*/
+
+SELECT card_name, MAX(issued_amount) - MIN(issued_amount) AS difference
+FROM monthly_cards_issued
+GROUP BY 1
+ORDER BY 2 DESC
+;
